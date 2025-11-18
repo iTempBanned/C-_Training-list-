953d4e9a5547ffe194bb6fe0b04de65b086b51f6
PC <pezzottimatteo985@gmail.com>
2025-11-18 21:22:03 +0100

    Push school simple exercise

--- main.cpp
+++ main.cpp
@@ -1,60 +1,25 @@
 #include <iostream>
+#include <list>
 using namespace std;
 
-template <typename T>
-void Scambia(T& a, T& b) {
-    T temp = a;
-    a = b;
-    b = temp;
-}
-
-template <typename T>
-void Maggiore(T a, T b) {
-    if (a > b) {
-        cout << a << " e' maggiore di " << b << endl;
-    } else if (b > a) {
-        cout << b << " e' maggiore di " << a << endl;
-    } else {
-        cout << a << " e " << b << " sono uguali" << endl;
-    }
-}
-
-template <typename T>
-void Stampa(T a, T b) {
-    cout << "=== Before Swap ===" << endl;
-    cout << "a = " << a << ", b = " << b << endl;
-
-    Scambia(a, b);
-
-    cout << "=== After Swap ===" << endl;
-    cout << "a = " << a << ", b = " << b << endl;
-
-    cout << "=== Comparison ===" << endl;
-    Maggiore(a, b);
-
-    // Removed recursive call that caused infinite loop
-}
-
 int main() {
-    cout << "****************************************" << endl;
-    cout << "*     Template Functions Demo          *" << endl;
-    cout << "****************************************" << endl;
+    int n;
+    list<string> l;
 
-    int a = 5, b = 10;
-    cout << "\nTesting with integers:" << endl;
-    Stampa(a, b);
+    cout << "Enter the number of elements: ";
+    cin >> n;
 
-    cout << "\nTesting with doubles:" << endl;
-    double x = 3.14, y = 2.71;
-    Stampa(x, y);
+    for (int i = 0; i < n; i++) {
+        string s;
+        cout << "Enter element " << i+1 << ": ";
+        cin >> s;
+        l.push_back(s);
+    }
 
-    cout << "\nTesting with characters:" << endl;
-    char char1 = 'A', char2 = 'Z';
-    Stampa(char1, char2);
-
-    cout << "\n****************************************" << endl;
-    cout << "*           Program Complete           *" << endl;
-    cout << "****************************************" << endl;
+    for (auto it = l.begin(); it != l.end(); it++) {
+        cout << *it << " ";
+    }
+    cout << endl;
 
     return 0;
 }
